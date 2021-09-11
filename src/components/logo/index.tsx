8f085b2553fec8d86e2050b2038586d5638c489f
kutaybuyukkorukcu <kutaybuyukkorukcu@gmail.com>
2021-09-11 10:36:56 +0300

    initial commit with superplate

---
+++ src/components/logo/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export const Logo: React.FC = () => {
+  return (
+    <img
+      data-test="icon"
+      src="/icons/react-icon.svg"
+      alt="react"
+      width="96"
+      height="58"
+    />
+  );
+};
